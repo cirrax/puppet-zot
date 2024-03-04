c13b6cc7deaea7f829a109ad0cb6c17bed4663eb
Benedikt Trefzer <benedikt.trefzer@cirrax.com>
2024-03-04 22:32:20 +0100

    add spec test for zot::config

---
+++ spec/classes/config_spec.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+describe 'zot::config' do
+  on_supported_os.each do |os, os_facts|
+    context "on #{os}" do
+      let(:facts) { os_facts }
+      let(:pre_condition) do
+        [
+          # Fake assert_private function from stdlib to not fail within this test
+          'function assert_private () { }',
+          # include main class for parameter
+          'include zot',
+        ]
+      end
+
+      it { is_expected.to compile.with_all_deps }
+    end
+  end
+end
